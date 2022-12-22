fd2518a79180b93224e9bf800ecba51042f12625
antlogist <antlogist@yandex.ru>
2022-12-22 20:17:50 +0300

    Interface including fixed

--- StructuralPatterns/Bridge/Controllers/WidgetController.php
+++ StructuralPatterns/Bridge/Controllers/WidgetController.php
@@ -1,11 +1,11 @@
 <?php
 
+include_once('Interfaces/WidgetRealizationInterface.php');
 include_once('Models/Product.php');
 include_once('Models/Category.php');
-include_once('Interfaces/WidgetRealizatonInterface.php');
 include_once('Classes/WidgetAbstract.php');
-include_once('Classes/WidgetBigAbstract.php');
-include_once('Classes/WidgetMiddleAbstract.php');
+include_once('Classes/WidgetBigAbstraction.php');
+include_once('Classes/WidgetMiddleAbstraction.php');
 include_once('Classes/ProductWidgetRealization.php');
 include_once('Classes/CategoryWidgetRealization.php');
 include_once('Classes/BridgeDemo.php');
